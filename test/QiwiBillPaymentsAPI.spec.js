9226549cc84c5d8f95eac2792cd6c1924e45ffb7
Second To None <secondtonone.secondtonone@gmail.com>
2017-12-29 11:46:25 +0300

    Fix test

--- test/QiwiBillPaymentsAPI.spec.js
+++ test/QiwiBillPaymentsAPI.spec.js
@@ -1,6 +1,7 @@
+const crypto = require('crypto');
 const QiwiBillPaymentsAPI = require('../lib/QiwiBillPaymentsAPI.js');
 const chai = require('chai');
-/*const Browser = require('zombie');*/
+const puppeteer = require('puppeteer');
 const assert = chai.assert;
 
 
@@ -10,16 +11,14 @@ const qiwiApi = new QiwiBillPaymentsAPI(SECRET_KEY);
 
 
 
-const bill_id = '12345678';
-
-const refund_id = '0734';
+const bill_id = crypto.randomBytes(5).toString('hex');
 
 const public_key = '2tbp1WQvsgQeziGY9vTLe9vDZNg7tmCymb4Lh6STQokqKrpCC6qrUUKEDZAJ7mvFnzr1yTebUiQaBLDnebLMMxL8nc6FF5zfmGQnypdXCbQJqHEJW5RJmKfj8nvgc';
 
 const amount = 200;
 
 
-describe('qiwi api v3', async() => {
+describe('qiwi api v3', () => {
 
     try {
 
@@ -29,36 +28,27 @@ describe('qiwi api v3', async() => {
 
             const testLink = `https://oplata.qiwi.com/create?public_key=${public_key}&amount=${amount}&bill_id=${bill_id}`;
 
-            link = qiwiApi.createPaymentForm({ public_key, amount, bill_id });
-
-            console.log('Пока так :3 Перейдите по ссылке, потом запустите тест снова: ', link)
+            link = qiwiApi.createPaymentForm({
+                public_key,
+                amount,
+                bill_id
+            });
 
             assert.equal(link, testLink);
 
         });
 
-        describe('requests:', async() => {
+        describe('requests: ', function () {
 
-            /* const browser = new Browser(); */
+            this.timeout(3000);
 
-
-
-          /*  before((done) => {
-
-                browser.visit(link, () => {
-
-                    browser.window.matchMedia = () => {
-                        return {
-                            matches: false,
-                            addListener: function() {},
-                            removeListener: function() {}
-                        };
-                    };
-
-                    done();
-                });
+            before(async() => {
+                
+                const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+                await page.goto(link, { waitUntil: 'networkidle2' });
+                await browser.close();
             });
-*/
 
 
             it('returns valid bill status', async() => {
@@ -66,27 +56,27 @@ describe('qiwi api v3', async() => {
                 try {
 
                     const data = await qiwiApi.getStatus(bill_id);
-                    
-                    assert.equal( data.result_code, 'SUCCESS');
+
+                    assert.equal(data.result_code, 'SUCCESS');
 
                 } catch (e) {
                     throw e;
                 }
             });
 
-            it('cancel unpaid bill', async () =>  {
+            it('cancel unpaid bill', async() => {
 
-                 try {
+                try {
 
-                     const data = await qiwiApi.cancel(bill_id);
+                    const data = await qiwiApi.cancel(bill_id);
 
-                     assert.equal( data.result_code, 'SUCCESS');
+                    assert.equal(data.result_code, 'SUCCESS');
 
-                 } catch (e) {
-                     throw e;
-                 }
+                } catch (e) {
+                    throw e;
+                }
 
-             });
+            });
         });
 
     } catch (e) {
