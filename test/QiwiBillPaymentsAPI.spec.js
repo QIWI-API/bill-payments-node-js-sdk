783c94071127ab191e283253d1354950311419c6
Second To None <secondtonone.secondtonone@gmail.com>
2018-01-19 20:57:29 +0300

    Add generate uuid method

--- test/QiwiBillPaymentsAPI.spec.js
+++ test/QiwiBillPaymentsAPI.spec.js
@@ -1,27 +1,31 @@
-const crypto = require("crypto");
-const QiwiBillPaymentsAPI = require("../lib/QiwiBillPaymentsAPI.js");
-const chai = require("chai");
-const puppeteer = require("puppeteer");
+const crypto = require('crypto');
+const QiwiBillPaymentsAPI = require('../lib/QiwiBillPaymentsAPI.js');
+const chai = require('chai');
+const puppeteer = require('puppeteer');
 const assert = chai.assert;
 
 const SECRET_KEY =
-    "eyJ2ZXJzaW9uIjoicmVzdF92MyIsImRhdGEiOnsibWVyY2hhbnRfaWQiOjUyNjgxMiwiYXBpX3VzZXJfaWQiOjcxNjI2MTk3LCJzZWNyZXQiOiJmZjBiZmJiM2UxYzc0MjY3YjIyZDIzOGYzMDBkNDhlYjhiNTk5NmI5ZWQ2NmQwNzdkMTg5ZjMwYzZhY2RjMzg1In19";
+    'eyJ2ZXJzaW9uIjoicmVzdF92MyIsImRhdGEiOnsibWVyY2hhbnRfaWQiOjUyNjgxMiwiYXBpX3VzZXJfaWQiOjcxNjI2MTk3LCJzZWNyZXQiOiJmZjBiZmJiM2UxYzc0MjY3YjIyZDIzOGYzMDBkNDhlYjhiNTk5NmI5ZWQ2NmQwNzdkMTg5ZjMwYzZhY2RjMzg1In19';
 
 const qiwiApi = new QiwiBillPaymentsAPI(SECRET_KEY);
 
-const bill_id = crypto.randomBytes(5).toString("hex");
+const bill_id = crypto.randomBytes(5).toString('hex');
 
 const public_key =
-    "2tbp1WQvsgQeziGY9vTLe9vDZNg7tmCymb4Lh6STQokqKrpCC6qrUUKEDZAJ7mvFnzr1yTebUiQaBLDnebLMMxL8nc6FF5zfmGQnypdXCbQJqHEJW5RJmKfj8nvgc";
+    '2tbp1WQvsgQeziGY9vTLe9vDZNg7tmCymb4Lh6STQokqKrpCC6qrUUKEDZAJ7mvFnzr1yTebUiQaBLDnebLMMxL8nc6FF5zfmGQnypdXCbQJqHEJW5RJmKfj8nvgc';
 
-const amount = 200;
+const amount = 200.345;
 
-describe("qiwi api v3", () => {
+const extra_test = 'test';
+
+describe('qiwi api v3', () => {
     try {
-        let link = "";
+        let link = '';
 
-        it("creates payment form", () => {
-            const testLink = `https://oplata.qiwi.com/create?public_key=${public_key}&amount=${amount}&bill_id=${bill_id}`;
+        it('creates payment form', () => {
+            const testLink = `https://oplata.qiwi.com/create?public_key=${public_key}&amount=${parseFloat(
+                amount
+            ).toFixed(2)}&bill_id=${bill_id}`;
 
             link = qiwiApi.createPaymentForm({
                 public_key,
@@ -32,15 +36,15 @@ describe("qiwi api v3", () => {
             assert.equal(link, testLink);
         });
 
-        describe("requests: ", function() {
+        describe('requests: ', function () {
             this.timeout(6000);
 
             before(async () => {
                 const browser = await puppeteer.launch({
-                    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+                    args: ['--no-sandbox', '--disable-setuid-sandbox']
                 });
                 const page = await browser.newPage();
-                await page.goto(link, { waitUntil: "networkidle2" });
+                await page.goto(link, { waitUntil: 'networkidle2' });
                 await browser.close();
             });
 
@@ -56,21 +60,21 @@ describe("qiwi api v3", () => {
                 }
             }); */
 
-            it("returns valid bill status", async () => {
+            it('returns valid bill status', async () => {
                 try {
                     const data = await qiwiApi.getStatus(bill_id);
 
-                    assert.equal(data.result_code, "SUCCESS");
+                    assert.equal(data.result_code, 'SUCCESS');
                 } catch (e) {
                     throw e;
                 }
             });
 
-            it("cancel unpaid bill", async () => {
+                        it('cancel unpaid bill', async () => {
                 try {
                     const data = await qiwiApi.cancel(bill_id);
 
-                    assert.equal(data.result_code, "SUCCESS");
+                    assert.equal(data.result_code, 'SUCCESS');
                 } catch (e) {
                     throw e;
                 }
